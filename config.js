90a522962df9558a1ad31e96dc33c2b73907aa71
bopdz123 <91541314+bopdz123@users.noreply.github.com>
2021-09-29 03:11:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -14,4 +14,4 @@ const CONFIG = {
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h ra ngõ nha ae.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Okiiiii lun <3'
